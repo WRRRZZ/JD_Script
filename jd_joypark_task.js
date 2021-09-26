05880e3e29a7f7a952f939ddd49d714b075f16f2
WRRRZZ <79828804+WRRRZZ@users.noreply.github.com>
2021-09-26 20:49:44 +0800

    Update jd_joypark_task.js

--- jd_joypark_task.js
+++ jd_joypark_task.js
@@ -153,7 +153,7 @@ message = ""
           }
         } else if (task.taskType === 'SHARE_INVITE') {
           for (let j = 0; j < 5; j++) {
-            let resp = await apTaskDrawAward(167, 'SHARE_INVITE');
+            let resp = await apTaskDrawAward(261, 'SHARE_INVITE');
 
             if (!resp.success) {
               break
